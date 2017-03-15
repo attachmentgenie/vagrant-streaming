1565daf435fb6caabc7ed2d2e2a107252a43757d
Bram Vogelaar <attachmentgenie@inuits.eu>
2017-03-15 14:45:26 +0100

    adding hiera.yaml and default manifest

--- production/manifests/default.pp
+++ production/manifests/default.pp
@@ -1,4 +1,4 @@
-hiera_include('classes',['roles::bootstrap'])
+lookup('classes', Array[String], 'unique', ['roles::node']).include
 
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
